8aa95e312d75d1916ebcaef6ae4547a6e55c5828
fan <291950304@qq.com>
2024-09-19 17:10:25 +0800

    feat: icons component

---
+++ components/icons.tsx
@@ -0,0 +1,16 @@
+import {
+  LayoutDashboardIcon,
+  LogIn,
+  LucideIcon,
+  User,
+  User2Icon
+} from 'lucide-react';
+
+export type Icon = LucideIcon;
+
+export const Icons = {
+  login: LogIn,
+  dashboard: LayoutDashboardIcon,
+  user: User,
+  profile: User2Icon
+};
