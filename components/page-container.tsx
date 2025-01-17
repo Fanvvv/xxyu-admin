e03d291b6bd3a080d493142e072d5d3f8bc23be9
fan <291950304@qq.com>
2025-01-17 16:01:41 +0800

    chore: page container

---
+++ components/page-container.tsx
@@ -0,0 +1,26 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
+
+/**
+ * PageContainer component
+ * @param scrollable - if true, the page container will be scrollable
+ * @returns
+ */
+export function PageContainer({
+  children,
+  scrollable = false
+}: {
+  children: React.ReactNode;
+  scrollable?: boolean;
+}) {
+  return (
+    <>
+      {scrollable ? (
+        <ScrollArea className="h-[calc(100dvh-52px)]">
+          <div className="flex flex-1 p-4 md:px-6">{children}</div>
+        </ScrollArea>
+      ) : (
+        <div className="flex flex-1 p-4 md:px-6">{children}</div>
+      )}
+    </>
+  );
+}
